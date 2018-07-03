f8b0c6132aa52b9e31d4f81237e83226406611e9
Cao Yuan <robertcao@zepplabs.com>
2018-07-03 12:43:13 -0700

    Update fail login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to '/internal/dashboard'
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'internal/index'
     end
   end
 
